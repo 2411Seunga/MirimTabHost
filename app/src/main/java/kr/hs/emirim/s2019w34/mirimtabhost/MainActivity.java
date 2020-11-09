725ee59c22f4086ba2bba25c07b03c9adc24888a
LeeSeungA <seunga4170@gmail.com>
2020-11-09 19:33:41 +0900

    Complete TabHost

--- app/src/main/java/kr/hs/emirim/s2019w34/mirimtabhost/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w34/mirimtabhost/MainActivity.java
@@ -14,5 +14,19 @@ public class MainActivity extends TabActivity {
         setContentView(R.layout.activity_main);
 
         TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecMal = tabHost.newTabSpec("Mal").setIndicator("말티푸");
+        tabSpecMal.setContent(R.id.linear_maltifu);
+        tabHost.addTab(tabSpecMal);
+
+        TabHost.TabSpec tabSpecPome = tabHost.newTabSpec("Pome").setIndicator("포메라니안");
+        tabSpecPome.setContent(R.id.linear_pome);
+        tabHost.addTab(tabSpecPome);
+
+        TabHost.TabSpec tabSpecHowl = tabHost.newTabSpec("Howl").setIndicator("하울");
+        tabSpecHowl.setContent(R.id.linear_howl);
+        tabHost.addTab(tabSpecHowl);
+
+        tabHost.setCameraDistance(0);
     }
 }
